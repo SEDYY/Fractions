f98efa6ca04bb77226220953e66d05d937e627bf
CaJIaT <90114253+SEDYY@users.noreply.github.com>
2025-05-15 12:22:09 +0300

    docx update

--- FractionWindowsApp/FractionWindowsApp/obj/Debug/net8.0-windows/FractionWindowsApp.AssemblyInfo.cs
+++ FractionWindowsApp/FractionWindowsApp/obj/Debug/net8.0-windows/FractionWindowsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FractionWindowsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30754dad31c8096f5d9a8c84d88f96ca45a0565")]
 [assembly: System.Reflection.AssemblyProductAttribute("FractionWindowsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FractionWindowsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
